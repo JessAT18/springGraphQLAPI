13f9371abebb866630cc168693893e8dc0cc20a1
JessAT18 <j-aquino@outlook.com>
2022-09-01 22:54:25 -0400

    Creacion del data fetcher

--- src/main/java/com/jessat18/springgraphqlapi/graphql/GraphQLProvider.java
+++ src/main/java/com/jessat18/springgraphqlapi/graphql/GraphQLProvider.java
@@ -2,11 +2,9 @@ package com.jessat18.springgraphqlapi.graphql;
 
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.schema.idl.*;
 import graphql.schema.idl.errors.SchemaProblem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
@@ -17,6 +15,8 @@ import java.io.IOException;
 
 @Component
 public class GraphQLProvider {
+    @Autowired
+    private GraphQLDataFetchers graphQLDataFetchers;
     @Value("classpath:schema.graphqls")
     private Resource resource;
     private GraphQL graphQL;
@@ -30,7 +30,14 @@ public class GraphQLProvider {
         final TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(resource.getInputStream());
         SchemaGenerator schemaGenerator = new SchemaGenerator();
 
-        return schemaGenerator.makeExecutableSchema(typeRegistry, RuntimeWiring.newRuntimeWiring().build());
+        return schemaGenerator.makeExecutableSchema(typeRegistry, buildWiring());
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(TypeRuntimeWiring.newTypeWiring("Query")
+                .dataFetcher("findAll", graphQLDataFetchers.getFindAllDataFetcher()))
+                .build();
     }
 
     @Bean
