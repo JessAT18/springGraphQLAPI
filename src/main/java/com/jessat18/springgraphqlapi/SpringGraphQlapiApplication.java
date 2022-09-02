e84f1cfb73b14ab4c3b10363455123e2607a698d
JessAT18 <j-aquino@outlook.com>
2022-09-01 22:35:26 -0400

    Initial commit

---
+++ src/main/java/com/jessat18/springgraphqlapi/SpringGraphQlapiApplication.java
@@ -0,0 +1,13 @@
+package com.jessat18.springgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphQlapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphQlapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessat18/springgraphqlapi/SpringGraphQlapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessat18.springgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
